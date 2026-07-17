512e4f743ccbcfc37a6ebc0a4a01fa163174e053
AMIR-art420 <amyrmhdyrjby417@gmail.com>
2026-07-17 10:45:59 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,16 @@
+import "./globals.css";
+
 export const metadata = {
   title: "MIGMIG Gaming",
   description: "تیم MIGMIG games",
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
