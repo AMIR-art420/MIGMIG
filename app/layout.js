833e7ba9c658e44d953dfe5a209b4d54634439fe
AMIR-art420 <amyrmhdyrjby417@gmail.com>
2026-07-17 10:48:03 -0700

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
+      <head>
+        <link rel="stylesheet" href="/assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="/assets/css/fontawesome.css" />
+        <link rel="stylesheet" href="/assets/css/templatemo-lugx-gaming.css" />
+        <link rel="stylesheet" href="/assets/css/owl.css" />
+        <link rel="stylesheet" href="/assets/css/animate.css" />
+        <link rel="stylesheet" href="/assets/css/fonts.css" />
+      </head>
+
       <body>
         {children}
       </body>
